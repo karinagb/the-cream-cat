67822d0b02d4b4f0fa3c13010d7025a60bc0a148
karinagb <karin.gb27@gmail.com>
2026-07-21 16:35:14 -0600

    updating .nojekyll

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/the-cream-cat" : "",
-    images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
