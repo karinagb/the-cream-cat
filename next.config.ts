4dcb64691d7f51602f9f1be292fe580055a3b609
karinagb <karin.gb27@gmail.com>
2026-07-21 15:18:58 -0600

    testing path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/the-cream-cat",
+  basePath: process.env.NODE_ENV === "production" ? "/the-cream-cat" : "",
 };
 
 export default nextConfig;
